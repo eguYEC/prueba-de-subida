562f51daf0988883aae9c5623bcfe966db616ed2
SCPC 501 <SCPC501@labs.upds>
2024-12-10 20:15:05 -0400

    se agrega la carpeta docs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { AcercadeComponent } from './pages/acercade/acercade.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+//import { HttpClientModule } from '@angular/common/http';
+//import { TemplateComponent } from './pages/template/template.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    AcercadeComponent,
+    NavbarComponent,
+    FooterComponent,
+    CapitalizadoPipe,
+   // TemplateComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    //HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/template/template.component.ts
@@ -0,0 +1,22 @@
+/*import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-template',
+  standalone: false,
+  
+  templateUrl: './template.component.html',
+  styleUrl: './template.component.css'
+})
+export class TemplateComponent {
+  paises: any[] = [];
+  constructor( private paisService: PaisService){}
+  ngOnInit(): void{
+    .subscribe( paises => {
+      this.paises = paises;
+      this.paises.unshift({nombre: '[ Seleccione Pais]',codigo: ''})
+    });
+  }
+
+
+}*/
